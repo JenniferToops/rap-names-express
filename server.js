97ea8d17093789267311fcee04bd32315a9faa7a
Jennifer Toops <jennifertoops@gmail.com>
2021-04-01 14:08:39 -0400

    commit occ-rap-names-express.herokuapp.com

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(express.static('public'))
+app.use(express.static('files'))
 
 
 app.get('/',(request, response)=>{
